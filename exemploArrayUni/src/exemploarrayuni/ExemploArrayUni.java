8c10dd091cfae2ac33cb713a98825cb402668ab2
professor <professor>
2022-10-25 21:23:09 -0300

    Parte II.I

--- exemploArrayUni/src/exemploarrayuni/ExemploArrayUni.java
+++ exemploArrayUni/src/exemploarrayuni/ExemploArrayUni.java
@@ -1,6 +1,7 @@
 package exemploarrayuni;
 
 import java.text.DecimalFormat;
+import java.util.Arrays;
 import javax.swing.JOptionPane;
 
 /**
@@ -17,6 +18,11 @@ public class ExemploArrayUni {
         int aux;
         DecimalFormat df = new DecimalFormat();
         df.applyPattern("000");
+        
+        float[] notas = {8.5f, 4.33f, 8.42f, 99.54f};
+        String[] alunos = {"João", "Maria", "Pedro", "Luiza"};
+        String nome, mensagem; 
+                
         for(int i=0; i<10; i++){
             aux = Integer.valueOf(JOptionPane.showInputDialog("Informe um número"));
             lista[i] = aux;
@@ -30,6 +36,32 @@ public class ExemploArrayUni {
         for(int i=0; i<20; i++){
             System.out.println(df.format(lista[i]));
         }
+        
+        for(int i=0; i<4; i++){
+            System.out.println("Aluno:" + alunos[i] + " - Nota: " + notas[i]);
+        }
+        
+        Arrays.sort(alunos);
+        
+        for(int i=0; i<4; i++){
+            System.out.println(alunos[i]);
+        }
+        
+        
+        //Busca em array
+        nome = JOptionPane.showInputDialog("Informe um nome");
+        mensagem = "Não encontrado";
+        
+        for(String valor: alunos){
+            if(valor.equalsIgnoreCase(nome)){
+                mensagem = "Aluno encontrado!";
+                break;
+            }
+        }
+        
+        System.out.println(mensagem);
+        
+        
     }
     
 }
